98cf721a89e9fb677ab3da95c8a15f30077d3f7b
KevinW2004 <221250068@smail.nju.edu.cn>
2024-11-23 23:16:20 +0800

    feat: 论文搜索和获取一篇具体论文
    - 由于论文没有id字段，所以获取时是使用title的全名

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from config import Config
 from models import db
+from services import *
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/papers/search', methods=['GET'])
+def search_papers():
+    keyword = request.args.get('keyword')
+    print("开始搜索，keyword: ", keyword)
+    if not keyword:
+        return jsonify({"error": "Keyword is required"}), 400
+    results = PaperService.search_papers(keyword)
+    return jsonify(results), 200
+
+@app.route('/papers/get_by_title', methods=['GET'])
+def get_paper_by_title():
+    title = request.args.get('title')
+    print("开始获取，title: ", title)
+    if not title:
+        return jsonify({"error": "Title is required"}), 400
+    result = PaperService.get_paper_by_title(title)
+    return jsonify(result), 200
 
 
 if __name__ == '__main__':
--- services.py
+++ services.py
@@ -1,4 +1,5 @@
 from models import db, User
+import pandas as pd
 
 class UserService:
     @staticmethod
@@ -8,5 +9,20 @@ class UserService:
         db.session.commit()
         return user
 
+
+
 class PaperService:
-    pass
+    dataset_path = './dataset/papers.csv'
+    papers = pd.read_csv(dataset_path)
+
+    @staticmethod
+    def search_papers(keyword):
+        # 根据标题搜索论文
+        results = PaperService.papers[PaperService.papers['title'].str.contains(keyword, case=False, na=False)]
+        return results.to_dict(orient='records')
+    
+    @staticmethod
+    def get_paper_by_title(title):
+        # 根据标题获取论文, 这里仅仅获取第一条
+        result = PaperService.papers[PaperService.papers['title'] == title]
+        return result.to_dict(orient='records')[0]
